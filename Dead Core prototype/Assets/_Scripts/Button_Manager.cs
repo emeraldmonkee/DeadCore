f0ae519895cec92218e744e2dc9e3e9788199c0d
Matthew Stageman <mattstageman@gmail.com>
2018-05-26 12:15:48 +0100

    Basic menu

---
+++ Dead Core prototype/Assets/_Scripts/Button_Manager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button_Manager : MonoBehaviour
+{
+    public void Start_Btn()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Settings_Btn()
+    {
+        //Settings code
+    }
+
+    public void Quit_Btn()
+    {
+        Application.Quit();
+    }
+}
--- Dead Core prototype/Assets/_Scripts/Move_Script.cs
+++ Dead Core prototype/Assets/_Scripts/Move_Script.cs
@@ -13,7 +13,6 @@ public class Move_Script : MonoBehaviour
 
     void Start()
     {
-        CharacterController controller = GetComponent<CharacterController>();
         playerCam = FindObjectOfType<Camera>();
     }
 
