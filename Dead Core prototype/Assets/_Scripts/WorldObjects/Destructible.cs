7b3e64f0cd321c97b71cc260bca72d7765a76c33
Jeffrey Lu <jefflu@live.ca>
2018-05-31 02:40:20 -0400

    Add world object interfaces
    Added some interfaces for world object use (e.g. buttons, barrels, walls, doors, stuff you can interact with in the world)
    Changed ColorSpace from Gamma to Linear; this can be changed if you guys don't like what it looks like (usually people prefer Linear)

---
+++ Dead Core prototype/Assets/_Scripts/WorldObjects/Destructible.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Destructible : MonoBehaviour, IDestroyable, IDamageable<float>
+{
+    public float StartHealth { get; set; }
+    public float CurrentHealth { get; set; }
+
+    private void Start()
+    {
+        CurrentHealth = StartHealth;
+    }
+
+    public virtual void TakeDamage(float amount)
+    {
+        CurrentHealth -= amount;
+        if (CurrentHealth <= 0)
+            Destroy();
+    }
+
+    public virtual void Destroy()
+    {
+        GameObject.Destroy(gameObject);
+    }
+}
---
+++ Dead Core prototype/Assets/_Scripts/WorldObjects/WorldObjectInterfaces.cs
@@ -0,0 +1,16 @@
+﻿public interface IDestroyable
+{
+    void Destroy();
+}
+
+public interface IDamageable<T>
+{
+    T StartHealth { get; set; }
+    T CurrentHealth { get; set; }
+    void TakeDamage(T amount);
+}
+
+public interface IInteractable
+{
+    void Interact();
+}
